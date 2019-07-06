12179fd5c19c819592b65affd285069bb03a9bda
Keira Dueck <sylae@calref.net>
2019-07-06 04:19:34 -0700

    also push to host

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@
 rm *.png
 inkscape -e "protectoratemon map - barov.svg.png" -d 150 "protectoratemon map - barov.svg"
 optipng "protectoratemon map - barov.svg.png"
+scp "protectoratemon map - barov.svg.png" keira@syl.ae:~/public_html/
